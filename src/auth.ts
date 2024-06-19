820b70b6471eab69739f46fb4632e01f4585226f
Hadil Guerfi <hdlguerfi@gmail.com>
2024-06-19 13:29:58 +0100

    accessing to role from session

--- src/auth.ts
+++ src/auth.ts
@@ -1,9 +1,51 @@
-import NextAuth from "next-auth";
+import NextAuth, { type DefaultSession } from "next-auth";
 import authConfig from "./auth.config";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import db from "@/lib/db";
+import { getUserById } from "../data/user";
+import { JWT } from "next-auth/jwt";
 
-export const { handlers, auth , signIn,signOut } = NextAuth({
+
+declare module "next-auth" {
+  interface Session {
+    user: {
+      id:string,
+      role: "USER"|"ADMIN";
+    } & DefaultSession["user"];
+  }
+}
+
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    role?: "USER" | "ADMIN";
+  }
+}
+
+
+
+export const { handlers, auth, signIn, signOut } = NextAuth({
+  callbacks: {
+    async jwt({ token }) {
+      if(!token.sub)return token;
+      const existingUser=await getUserById(token.sub)
+      if(!existingUser) return token;
+      token.role=existingUser.role
+      return token;
+    },
+
+    async session({ token, session }) {
+      console.log({ sessionToken: token });
+      if (token.sub && session.user) {
+        session.user.id = token.sub;
+      }
+      if (token.role && session.user) {
+        session.user.role = token.role;
+      }
+      return session;
+    },
+  },
+
   adapter: PrismaAdapter(db),
   session: { strategy: "jwt" },
   ...authConfig,
